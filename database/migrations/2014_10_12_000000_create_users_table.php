d7df4724952947c5663f97963244373fc3455dc0
subhajit saha <saha3475@gmail.com>
2024-01-26 22:59:29 +0530

    initial commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Egulias\EmailValidator\Parser\Comment;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('image')->nullable();
             $table->rememberToken();
+            $table->tinyInteger('status')->default(0)->Comment('0=>inactive,1=>active');
             $table->timestamps();
             $table->softDeletes();
         });
